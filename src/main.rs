66e5f7c799770d2ef792b41d85435cdb3ad009a5
Gabriele Mambrini <g.mambrini@gmail.com>
2021-08-07 17:42:32 +0200

    first experiments

---
+++ src/main.rs
@@ -0,0 +1,100 @@
+use git2::Repository;
+use std::env;
+use structopt::StructOpt;
+use termion::color;
+
+#[derive(StructOpt, Debug)]
+struct Opts {
+    // The name of the remote
+    #[structopt(long)]
+    remote: Option<String>,
+
+    // The name of the default branch (e.g. main or master)
+    #[structopt(long)]
+    default_branch: Option<String>,
+
+    #[structopt(subcommand)]
+    // Note that we mark a field as a subcommand
+    cmd: Option<Command>,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    Show,
+    Prev,
+    Next,
+}
+
+struct Context {
+    repo: Repository,
+}
+
+fn main() {
+    let opts = Opts::from_args();
+
+    let path = env::current_dir().expect("Cannot find current directory. Giving up.");
+    let repo = match Repository::open(&path) {
+        Ok(repo) => repo,
+        Err(e) => panic!("No git repo found at {:?}: {}", path, e),
+    };
+
+    let ctx = Context { repo };
+
+    match opts.cmd.unwrap_or(Command::Show) {
+        Command::Show => cmd_show(ctx).unwrap(),
+        _ => println!("Not implemented"),
+    }
+}
+
+fn cmd_show(ctx: Context) -> Result<(), git2::Error> {
+    println!(
+        "{}State:{}\t {:?}",
+        color::Fg(color::Green),
+        color::Fg(color::Reset),
+        ctx.repo.state(),
+    );
+
+    let remotes: Vec<String> = ctx
+        .repo
+        .remotes()
+        .unwrap()
+        .iter()
+        .map(|r| r.unwrap().to_string())
+        .collect();
+    println!(
+        "{}Remotes:{}\t{}",
+        color::Fg(color::Green),
+        color::Fg(color::Reset),
+        remotes.join(",")
+    );
+
+    let mut revwalk = ctx.repo.revwalk()?;
+
+    // Prepare the revwalk
+    revwalk.set_sorting(git2::Sort::NONE | git2::Sort::TIME)?;
+    revwalk.push_head()?;
+
+    let revwalk = revwalk.filter_map(|id| {
+        let id = if let Ok(id) = id { id } else { return None };
+        let commit = ctx.repo.find_commit(id).map_err(|e| Some(e));
+        Some(commit)
+    });
+
+    // print!
+    for commit in revwalk {
+        if let Ok(commit) = commit {
+            println!(
+                "{} {}",
+                commit.id(),
+                String::from_utf8_lossy(commit.message_bytes())
+                    .lines()
+                    .next()
+                    .unwrap()
+            );
+        } else {
+            println!("{:?}", commit);
+        }
+    }
+
+    Ok(())
+}
